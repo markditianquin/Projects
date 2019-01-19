81c35bc3652455dd89369056beb873faa94e75ec
markditianquin <markditianquin@gmail.com>
2019-01-18 19:16:18 -0800

    On to Heroes and HTTP

--- Angular/angular-tour-of-heroes/src/app/app.module.ts
+++ Angular/angular-tour-of-heroes/src/app/app.module.ts
@@ -4,6 +4,10 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
+
 import { FormsModule } from '@angular/forms';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
@@ -15,7 +19,8 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     HeroesComponent,
     HeroDetailComponent,
     MessagesComponent,
-    DashboardComponent
+    DashboardComponent,
+    HttpClientModule
   ],
   imports: [
     BrowserModule,
